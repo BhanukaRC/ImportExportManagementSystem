b336481241395bc5e77feebf0d212aa5290d5a10
BhanukaRC <bhanuka.15@cse.mrt.ac.lk>
2017-06-28 11:24:16 +0530

    jkb

---
+++ ImportExportManagement/ImportExportManagement/DatabaseAccess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ImportExportManagement
+{
+    class DatabaseAccess
+    {
+        private static MySqlConnection connection;
+                
+
+        public static MySqlConnection getConnection() {
+            
+            if (connection == null)
+            {
+                connection = new MySqlConnection(ConnectionConfiguration.ConnectionString("IEManagement"));
+            }
+            return connection;
+               
+            
+        }
+    }
+}
